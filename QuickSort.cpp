9e3117574b7816c0d4f58d7dd306875358b223bb
Md. Naimul Islam <78869917+Nayeem-co@users.noreply.github.com>
2022-07-18 11:08:07 +0600

    Add files via upload

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,5 +1,15 @@
+#include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
+
+
+void print(int arr[], int n){
+    for(int i=0 ; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    cout<<endl;
+}
 
 int partitiaion(int arr[], int p, int r){
     int i = p-1;
@@ -11,11 +21,13 @@ int partitiaion(int arr[], int p, int r){
         }
     }
     swap(arr[i+1] , arr[r]);
+    print(arr,6);
+    cout<<"q: "<<i+1<<endl;
     return i+1;
 }
 
 void Quicksort(int arr[], int p , int r){
-    if(r < p){
+    if(r <= p){
         return;
     }
     int q = partitiaion(arr,p,r);
@@ -26,9 +38,10 @@ void Quicksort(int arr[], int p , int r){
 int main(int argc, char const *argv[])
 {
 
-    int arr[5] = {4,5,1,2,3};
-    Quicksort(arr,0,4);
-    for(int i=0; i< 5 ; i++){
+    int arr[6] = {7,3,1,8,9,4};
+    Quicksort(arr,0,5);
+    cout<<"final output: "<<endl;
+    for(int i=0; i< 6 ; i++){
         cout<<arr[i]<<endl;
     }
     
---
+++ powerOfNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+int power(int x,int y){
+    if(y == 0){
+        return 1;
+    }
+    int temp = power(x,y/2);
+    if(y % 2 == 0){
+        return temp*temp;
+    }
+    else{
+        return x*temp*temp;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int x,y;
+    cin>>x>>y;
+
+    cout<<power(x,y);
+    
+    return 0;
+}
